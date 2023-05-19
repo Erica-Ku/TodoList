0a6b62029b88dd65e6ebac59f7d8be7f2509f174
Erica-Ku <yeskuminji@naver.com>
2023-05-19 12:34:06 +0900

    삭제 기능 구현

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,15 @@
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, updateTodo, deleteTodo }) => {
+  const handleDelete = id => {
+    deleteTodo(id);
+  };
+
   return (
     <ul>
       {todos.map(todo => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => handleDelete(todo.id)}>Delete</button>
+        </li>
       ))}
     </ul>
   );
